34e246356b31560029f54f15f8df347e69e124a1
Denon Simpson <47371356+DJS178@users.noreply.github.com>
2020-10-13 17:55:12 -0400

    First Check-In

---
+++ Assets/Calculator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calculator : MonoBehaviour
+{
+
+    public GameObject cube;
+    public float interestRate;
+    Transform cube_transform;
+    Vector3 currentVolumeVector;
+    float cubeLength,cubeHeight,cubeWidth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            CalculateInterest();
+        }
+    }
+
+
+    public void CalculateInterest() {
+        Transform transform = cube.transform;
+        float cubeLength = transform.localScale.x;
+        float cubeWidth = transform.localScale.z;
+        float cubeHeight = transform.localScale.y;
+
+        cubeLength *= interestRate;
+        cubeWidth *= interestRate;
+        cubeHeight *= interestRate;
+
+        transform.localScale = new Vector3(cubeLength,cubeHeight,cubeWidth);
+        float newVolume = cubeLength * cubeHeight * cubeWidth;
+        Debug.Log("New Volume: " + newVolume);
+
+    }
+}
